93a72afb27981877bef90040d7eea84520a70fbf
Mike Mudge <mike.mudge@gmail.com>
2019-11-03 15:12:53 +1300

    Build the graph of nanobots.

--- puzzle23/puzzle.php
+++ puzzle23/puzzle.php
@@ -6,9 +6,9 @@ $data = file_get_contents($filename);
 $lines = explode("\n", $data);
 
 $nanoBots = [];
-$maxRange;
+$maxRange = 0;
 $maxRangeNanoBot;
-foreach($lines as $line) {
+foreach($lines as $i=>$line) {
   echo($line. "\n");
   $parts = explode(",", substr($line, 5));
   $x = intval($parts[0]);
@@ -18,6 +18,7 @@ foreach($lines as $line) {
   $r = intval(substr($parts[3], 3));
 
   $nanoBot = [
+    'id' => $i,
     'x' => $x,
     'y' => $y,
     'z' => $z,
@@ -47,6 +48,47 @@ echo("Part 1: $numNanobots\n");
 
 // TODO figure out how to find the location with most nanobots in range?
 // There are only 1000 nanobots. But the number of coordinates is very large.
-// Use some kind of area mapping to locate positions?
+class Node {
+  private $data;
+  private $edges;
 
-// echo("Part 2: " . . "\n");
+  function __construct($data) {
+    $this->data = $data;
+    $this->edges = [];
+  }
+
+  function addEdge($node) {
+    $this->edges[] = $node;
+  }
+}
+
+// First lets find bot which share a region in range.
+foreach($nanoBots as $id=>$n) {
+  $nodes[$n['id']] = new Node($n);
+}
+print_r($nodes);
+
+foreach($nanoBots as $n1) {
+  $node = $nodes[$n1['id']];
+  foreach($nanoBots as $n2) {
+    if ($n2['id'] == $n1['id']) {
+      continue;
+    }
+    $dis = abs($n1['x'] - $n2['x']);
+    $dis += abs($n1['y'] - $n2['y']);
+    $dis += abs($n1['z'] - $n2['z']);
+    if ($dis <= $n1['r'] + $n2['r']) {
+      $node2 = $nodes[$n2['id']];
+      $node->addEdge($node2);
+      $node2->addEdge($node);
+      echo($n1['id'] . " can see " . $n2['id'] . "\n");
+    }
+  }
+}
+
+// Find the biggest clique?
+foreach ($nodes as $node) {
+  // Clique finder?
+  // for each child see if there is a clique?
+
+}
